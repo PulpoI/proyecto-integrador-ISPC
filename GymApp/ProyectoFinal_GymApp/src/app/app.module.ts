7d87e1cd1e3c79f4679d49e7b1a7df8dc3ac2f14
Sole1988 <sole.mattos2@gmail.com>
2023-05-03 20:28:50 -0300

    estructura/routing

--- GymApp/ProyectoFinal_GymApp/src/app/app.module.ts
+++ GymApp/ProyectoFinal_GymApp/src/app/app.module.ts
@@ -3,11 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistroComponent } from './auth/registro/registro.component';
+import { ClasesComponent } from './pages/clases/clases.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { PlanesComponent } from './pages/planes/planes.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavComponent } from './shared/nav/nav.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    ClasesComponent,
+    ContactoComponent,
+    DashboardComponent,
+    HomeComponent,
+    NosotrosComponent,
+    PlanesComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
