f9558332dbb3b7ddd411dd49ecca4bc1f3bfe175
Sole1988 <sole.mattos2@gmail.com>
2023-05-03 21:39:24 -0300

    routing

--- GymApp/ProyectoFinal_GymApp/src/app/app-routing.module.ts
+++ GymApp/ProyectoFinal_GymApp/src/app/app-routing.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//pages
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { PlanesComponent } from './pages/planes/planes.component';
+import { ClasesComponent } from './pages/clases/clases.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistroComponent } from './auth/registro/registro.component';
 
 
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent},
   {path:'inicio', component: DashboardComponent},
+  {path:'nosotros', component:NosotrosComponent},
+  {path:'planes', component: PlanesComponent},
+  {path:'clases', component: ClasesComponent},
+  {path:'contacto', component:ContactoComponent},
+  {path:'login', component: LoginComponent},
+  {path:'registro', component: RegistroComponent},
+  {path:'**', component: HomeComponent}
+
 ];
 
 @NgModule({
