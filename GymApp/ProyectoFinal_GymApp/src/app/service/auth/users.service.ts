9fd5b7448e1451e4a639503ac2cc851a578c917b
Sole1988 <sole.mattos2@gmail.com>
2023-05-20 16:24:57 -0300

    componente de registro

--- GymApp/ProyectoFinal_GymApp/src/app/service/auth/users.service.ts
+++ GymApp/ProyectoFinal_GymApp/src/app/service/auth/users.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Usuarios } from 'src/app/user';
 
 @Injectable({
   providedIn: 'root'
