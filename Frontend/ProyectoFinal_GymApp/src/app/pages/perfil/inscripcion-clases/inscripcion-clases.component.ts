307a0d5339b37b5bd79bc717c91b033db77c3970
Sole1988 <sole.mattos2@gmail.com>
2023-06-08 21:22:06 -0300

    inscripcion a clases

--- Frontend/ProyectoFinal_GymApp/src/app/pages/perfil/inscripcion-clases/inscripcion-clases.component.ts
+++ Frontend/ProyectoFinal_GymApp/src/app/pages/perfil/inscripcion-clases/inscripcion-clases.component.ts
@@ -1,6 +1,8 @@
+
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from 'src/app/service/auth.service';
+import { ClientesService } from 'src/app/service/clientes.service';
 
 @Component({
   selector: 'app-inscripcion-clases',
@@ -8,67 +10,68 @@ import { AuthService } from 'src/app/service/auth.service';
   styleUrls: ['./inscripcion-clases.component.css']
 })
 export class InscripcionClasesComponent implements OnInit {
+
   clases: any[] = [];
-  clasesInscritas: any[] = [];
-  claseSeleccionada: number = 0;
+  reservas: any[] = [];
+  usuarioId!: number; // ID del usuario actualmente autenticado
 
   constructor(
     private http: HttpClient,
-    private authService: AuthService
+    private authService: AuthService,
+    private clientesService: ClientesService
   ) {}
 
   ngOnInit(): void {
-    this.obtenerClases();
-    const clienteId = this.authService.obtenerIdCliente();
-    this.obtenerClasesInscritas(clienteId);
+    this.getClases();
+    this.getReservas();
+
+    // Obtener el ID del cliente autenticado
+    const isAdmin = this.authService.getIsAdmin();
+    const clienteId = isAdmin ? this.authService.obtenerIdClienteAdmin() : this.authService.obtenerIdCliente();
+
+    // Asignar el ID del cliente al usuarioId
+    this.usuarioId = clienteId;
   }
 
-  obtenerClases(): void {
-    this.http.get('http://127.0.0.1:8000/api/clases/').subscribe(
-      (response: any) => {
-        if (response.mensaje === 'Success') {
-          this.clases = response.clases;
-        } else {
-          console.log('No se encontraron clases');
-        }
-      },
-      (error) => {
-        console.error('Error al obtener las clases:', error);
+  getClases() {
+    this.http.get<any>('http://127.0.0.1:8000/api/clases').subscribe(response => {
+      if (response.mensaje === 'Success') {
+        this.clases = response.clases;
       }
-    );
+    });
   }
 
-  obtenerClasesInscritas(clienteId: number): void {
-    this.http.get('http://127.0.0.1:8000/api/reservas/?cliente_id=' + clienteId).subscribe(
-      (response: any) => {
-        if (response.mensaje === 'Success') {
-          this.clasesInscritas = response.reservas;
-        } else {
-          console.log('No se encontraron reservas');
-        }
-      },
-      (error) => {
-        console.error('Error al obtener las reservas:', error);
+  getReservas() {
+    this.http.get<any>('http://127.0.0.1:8000/api/reservas').subscribe(response => {
+      if (response.mensaje === 'Success') {
+        this.reservas = response.reservas;
       }
-    );
+    });
   }
 
-  anotarse(): void {
-    const clienteId = this.authService.obtenerIdCliente();
-    const reserva = { cliente_id: clienteId, clase_id: this.claseSeleccionada };
-    this.http.post('http://127.0.0.1:8000/api/reservas/', reserva).subscribe(
-      (response: any) => {
-        if (response.mensaje === 'Success') {
-          console.log('Anotado exitosamente');
-          // Actualizar las clases inscritas después de realizar la reserva
-          this.obtenerClasesInscritas(clienteId);
-        } else {
-          console.log('Error al anotarse:', response.mensaje);
-        }
-      },
-      (error) => {
-        console.error('Error al anotarse:', error);
+  inscribirse(clase: any) {
+    // Realizar la solicitud de inscripción a la API
+    this.http.post<any>('http://127.0.0.1:8000/api/reservas', { cliente_id: this.usuarioId, clase_id: clase.id }).subscribe(response => {
+      if (response.mensaje === 'Success') {
+        // Inscripción exitosa, actualizar las reservas
+        this.getReservas();
+      } else {
+        // Manejar el caso en que ocurra un error en la inscripción
+        console.log('Error al inscribirse');
       }
-    );
+    });
+  }
+
+  cancelarReserva(reserva: any) {
+    // Realizar la solicitud de cancelación de reserva a la API
+    this.http.delete<any>('http://127.0.0.1:8000/api/reservas/' + reserva.id).subscribe(response => {
+      if (response.mensaje === 'Success') {
+        // Cancelación exitosa, actualizar las reservas
+        this.getReservas();
+      } else {
+        // Manejar el caso en que ocurra un error en la cancelación de reserva
+        console.log('Error al cancelar la reserva');
+      }
+    });
   }
 }
