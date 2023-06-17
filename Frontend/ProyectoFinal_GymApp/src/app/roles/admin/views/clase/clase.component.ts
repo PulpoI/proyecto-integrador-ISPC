46f99bba9d99232b43ee53fcf957fa5eaaec8bdc
CarolinaEMedinaArg <carolinaestefaniamedina@gmail.com>
2023-06-17 16:22:18 -0300

    #US24 v.2. sweet alert alerts de plan y clase eliminada

--- Frontend/ProyectoFinal_GymApp/src/app/roles/admin/views/clase/clase.component.ts
+++ Frontend/ProyectoFinal_GymApp/src/app/roles/admin/views/clase/clase.component.ts
@@ -1,14 +1,15 @@
 import { ApiService } from 'src/app/service/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clase',
   templateUrl: './clase.component.html',
   styleUrls: ['./clase.component.css']
 })
-
 export class ClaseComponent {
+  title = 'sweetAlert';
   clase: any;
 
   constructor(private route: ActivatedRoute, private router: Router, private apiService: ApiService) {}
@@ -16,21 +17,42 @@ export class ClaseComponent {
   ngOnInit(): void {
     this.clase = history.state.clase;
   }
+
   regresar(): void {
     this.router.navigate(['/dashboard']);
   }
-  eliminarClase(): void {
-    if (confirm('¿Estás seguro de que deseas eliminar esta clase?')) {
-      this.apiService.eliminarClase(this.clase.id).subscribe(
-        (response: any) => {
-          console.log('Clase eliminada exitosamente', response);
-          this.router.navigate(['/dashboard']);
-        },
-        (error: any) => {
-          console.error('Error al eliminar la clase', error);
-        }
-      );
-    }
+
+  showModal() {
+    Swal.fire({
+      title: 'Estás seguro?',
+      text: "No podrás deshacer este cambio!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, elimínala!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.eliminarClase();
+      }
+    });
   }
-  
+
+  eliminarClase(): void {
+    this.apiService.eliminarClase(this.clase.id).subscribe(
+      (response: any) => {
+        console.log('Clase eliminada exitosamente', response);
+        Swal.fire(
+          'eliminada!',
+          'Tu clase ha sido eliminada.',
+          'success'
+        ).then(() => {
+          this.router.navigate(['/dashboard']);
+        });
+      },
+      (error: any) => {
+        console.error('Error al eliminar la clase', error);
+      }
+    );
+  }
 }
--- Frontend/ProyectoFinal_GymApp/src/app/roles/admin/views/plan/plan.component.ts
+++ Frontend/ProyectoFinal_GymApp/src/app/roles/admin/views/plan/plan.component.ts
@@ -1,13 +1,14 @@
 import { ApiService } from 'src/app/service/api.service';
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.component.html',
   styleUrls: ['./plan.component.css']
 })
 export class PlanComponent {
+  title = 'sweetAlert';
   plan: any;
 
   constructor(private route: ActivatedRoute, private router: Router, private apiService: ApiService) {}
@@ -18,18 +19,41 @@ export class PlanComponent {
   regresar(): void {
     this.router.navigate(['/dashboard']);
   }
+
+  showModal() {
+    Swal.fire({
+      title: 'Estás seguro?',
+      text: "No podrás deshacer este cambio!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, elimínala!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.eliminarPlan();
+      }
+    });
+  }
+
   eliminarPlan(): void {
-    if (confirm('¿Estás seguro de que deseas eliminar este plan?')) {
+    
       this.apiService.eliminarPlan(this.plan.id).subscribe(
         (response: any) => {
           console.log('Plan eliminado exitosamente', response);
-          this.router.navigate(['/dashboard']);
+          Swal.fire(
+            'Eliminado!',
+            'Tu plan fue eliminado!.',
+            'success'
+          ).then(() => {
+            this.router.navigate(['/dashboard']);
+          });
         },
         (error: any) => {
-          console.error('Error al eliminar el plan', error);
+          console.error('Error al eliminarel plane', error);
         }
       );
-    }
+    
   }
   
 }
